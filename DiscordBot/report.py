6169755c8e8cb077aca9b1193c8221b9936d84be
ende <endeshen99@gmail.com>
2023-05-20 23:14:00 -0700

    completed reporting flow

--- DiscordBot/report.py
+++ DiscordBot/report.py
@@ -15,11 +15,19 @@ class Report:
     START_KEYWORD = "report"
     CANCEL_KEYWORD = "cancel"
     HELP_KEYWORD = "help"
+    ABUSE_TYPES = ["nudity", "hate speech", "harassment", "terrorism", "scam/fraud", "suicide/self-harm", "others"]
 
     def __init__(self, client):
         self.state = State.REPORT_START
         self.client = client
-        self.message = None
+        self.abuse_message = None
+        self.abuse_type = None
+
+    def produce_report(self, message = None):
+        print("\n\n                          REPORTED CONTENT                     \n")
+        print("someone reported this message: " + self.abuse_message.content + ". This messege is sent by user: " + self.abuse_message.author.name)
+        print("this content is being reported for: " + self.abuse_type)
+        if message is not None: print("the additional reported context is: " + message.content + "\n")
     
     async def handle_message(self, message):
         '''
@@ -58,23 +66,34 @@ class Report:
 
             # Here we've found the message - it's up to you to decide what to do next!
             self.state = State.MESSAGE_IDENTIFIED1
+            self.abuse_message = message
             return ["I found this message:", "```" + message.author.name + ": " + message.content + "```", \
                     "Why are you reporting this content?", \
-                      "Use `terrorism` for terrorism " ]
+                      "Please type in one of the keywords to identify the type of abuse", \
+                      "options: `nudity`, `hate speech`, `harassment`, `terrorism`, `scam/fraud`, `suicide/self-harm`, `others`" ]
         
         if self.state == State.MESSAGE_IDENTIFIED1:
-            self.state = State.AWAITING_MESSAGE2
-            return ["What does this post specifically do?", \
-                "Use `g&v` Graphic & Violent Content", \
-                   "Use `acts` Promotes/Supports Terrorist Acts", \
+            if message.content not in self.ABUSE_TYPES:
+                return ["I'm sorry, I couldn't understand that. Please try again from the options or say `cancel` to cancel."]
+            if message.content == 'terrorism':
+                self.state = State.AWAITING_MESSAGE2
+                self.abuse_type = 'terrorism'
+                return ["What does this post specifically do?", \
+                    "Use `g&v` Graphic & Violent Content", \
+                    "Use `acts` Promotes/Supports Terrorist Acts", \
                     "Use `recruitment` Terrorist Recruitment Content" ]
+            else:
+                self.state = State.MESSAGE_IDENTIFIED2
+                self.abuse_type = message.content
+                return ["Can you provide additional information?", \
+                "Use `yes` for yes ", \
+                   "Use `no` for no ", ] 
         
         if self.state == State.AWAITING_MESSAGE2:
-            print(message.content)
             if not (message.content == 'acts' or message.content == 'g&v' or message.content == 'recruitment'):
                 return ["I'm sorry, I couldn't understand that. Please try again from the options or say `cancel` to cancel."]
             else:
-                print("The content is being reported for " + message.content)
+                self.abuse_type += "--" + message.content
                 self.state = State.MESSAGE_IDENTIFIED2
                 return ["Can you provide additional information?", \
                 "Use `yes` for yes ", \
@@ -88,10 +107,15 @@ class Report:
                     self.state = State.AWAITING_MESSAGE3
                     return ["Please tell us the additional information"]
                 else:
+                    self.produce_report()
+                    self.state = State.REPORT_COMPLETE
                     return ["Thank you for reporting. Someone from our team will review this and get back to you shortly"]
         
         if self.state == State.AWAITING_MESSAGE3:
-            print("the additional content is " + message.content)
+
+            self.produce_report(message)
+            self.state = State.REPORT_COMPLETE
+
             return ["Thank you for reporting. Someone from our team will review this and get back to you shortly"]
 
             
